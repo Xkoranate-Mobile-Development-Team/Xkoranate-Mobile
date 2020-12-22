737fd4cf9518665cc9aba85b46f57700e91bcdcf
Tumi <oluwatumininuojo25@gmail.com>
2020-12-22 10:24:45 +0100

    Xkoranate

---
+++ app/src/main/java/com/xkoranate/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.xkoranate
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val isFirstRun: Boolean =
+                getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE).getBoolean("isFirstRun", true)
+
+        if (isFirstRun) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        } else {
+            startActivity(Intent(this, MainScreenActivity::class.java))
+            finish()
+        }
+
+        getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE).edit()
+                .putBoolean("isFirstRun", false).apply()
+    }
+}
---
+++ app/src/main/java/com/xkoranate/MainScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.xkoranate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
