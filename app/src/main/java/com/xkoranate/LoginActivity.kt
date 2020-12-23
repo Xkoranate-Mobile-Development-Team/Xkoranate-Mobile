20bfe2e90d10eed9d5ce563c4b9fd8beebb9c6f1
Tumi <oluwatumininuojo25@gmail.com>
2020-12-23 20:05:40 +0100

    Xkoranate

--- app/src/main/java/com/xkoranate/LoginActivity.kt
+++ app/src/main/java/com/xkoranate/LoginActivity.kt
@@ -1,14 +1,22 @@
 package com.xkoranate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.xkoranate.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var loginBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        loginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
-
+        loginBinding.signUpButtonForLoginActivity.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
