792d7850599c970a2ce084db9b5309e82b5f2b6b
Tumi <oluwatumininuojo25@gmail.com>
2021-02-13 07:08:25 +0100

    toolbar fix, gradle now 4.0.0, other layout updates

--- app/src/main/java/com/xkoranate/ui/activities/MainActivity.kt
+++ app/src/main/java/com/xkoranate/ui/activities/MainActivity.kt
@@ -2,29 +2,20 @@ package com.xkoranate.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.xkoranate.R
+import com.xkoranate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
-//        val isFirstRun: Boolean =
-//                getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE).getBoolean("isFirstRun", true)
-//
-//        if (isFirstRun) {
-//            startActivity(Intent(this, LoginActivity::class.java))
-//            finish()
-//        } else {
-////            startActivity(Intent(this, MainScreenActivity::class.java))
-//            startActivity(Intent(this, LoginActivity::class.java))
-//            finish()
-//        }
-//
-//        getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE).edit()
-//                .putBoolean("isFirstRun", false).apply()
+        binding.menuIcon.setOnClickListener {
+            binding.drawerLayout.open()
+        }
     }
 
     override fun onBackPressed() {
