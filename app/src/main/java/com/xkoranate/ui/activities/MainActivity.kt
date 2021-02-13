d65368b2c031822b338fcaa912ff5fee9ba03306
Tumi <oluwatumininuojo25@gmail.com>
2021-02-14 00:18:03 +0100

    navigation view changes

--- app/src/main/java/com/xkoranate/ui/activities/MainActivity.kt
+++ app/src/main/java/com/xkoranate/ui/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.xkoranate.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.xkoranate.R
 import com.xkoranate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,30 @@ class MainActivity : AppCompatActivity() {
         binding.menuIcon.setOnClickListener {
             binding.drawerLayout.open()
         }
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+
+                R.id.nav_match_day -> {
+//                    Navigation.findNavController().navigate(R.id.action_homeFragment_to_matchDayFragment2)
+                }
+                R.id.nav_result -> {
+                    // Stays in the same fragment
+                }
+                R.id.nav_settings -> {
+//                    Navigation.findNavController(binding.root).navigate(R.id.action_homeFragment_to_settingsFragment)
+                }
+                R.id.nav_log_out -> {
+                    // Log the user out of the app
+                }
+
+            }
+
+            binding.drawerLayout.close()
+
+            true
+        }
+
     }
 
     override fun onBackPressed() {
