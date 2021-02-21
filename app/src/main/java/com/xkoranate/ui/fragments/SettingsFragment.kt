a8030b1f8df604e412721d46a2c6ac1e919d3a81
Tumi <oluwatumininuojo25@gmail.com>
2021-02-21 18:18:44 +0100

    updates

--- app/src/main/java/com/xkoranate/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/xkoranate/ui/fragments/SettingsFragment.kt
@@ -4,19 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.xkoranate.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
     private var binding: FragmentSettingsBinding? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +18,7 @@ class SettingsFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSettingsBinding.inflate(inflater)
 
-        if (binding?.darkModeSwitch?.isChecked!!) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        }
+
 
         return binding?.root
     }
