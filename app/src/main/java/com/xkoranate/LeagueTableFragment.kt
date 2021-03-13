f85d2e0914d079f8c00c573e8f8ce49b77e86da0
Tumi <oluwatumininuojo25@gmail.com>
2021-03-13 05:28:21 +0100

    league table fragment

--- app/src/main/java/com/xkoranate/LeagueTableFragment.kt
+++ app/src/main/java/com/xkoranate/LeagueTableFragment.kt
@@ -5,17 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.xkoranate.databinding.FragmentLeagueTableBinding
 
 
 class LeagueTableFragment : Fragment() {
 
+    var binding: FragmentLeagueTableBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_league_table, container, false)
+        binding = FragmentLeagueTableBinding.inflate(inflater)
+
+
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 
 }
