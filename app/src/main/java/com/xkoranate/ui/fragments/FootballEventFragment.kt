59f6b1c886f6d5e47baa8251adbf0e08e5c52787
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2021-05-05 07:55:35 +0100

    updates

---
+++ app/src/main/java/com/xkoranate/ui/fragments/FootballEventFragment.kt
@@ -0,0 +1,19 @@
+package com.xkoranate.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.xkoranate.R
+
+class FootballEventFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_football_event, container, false)
+    }
+}
---
+++ app/src/main/java/com/xkoranate/ui/fragments/TennisEventFragment.kt
@@ -0,0 +1,20 @@
+package com.xkoranate.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.xkoranate.R
+
+class TennisEventFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tennis_event, container, false)
+    }
+
+}
